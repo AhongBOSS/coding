c8b86228980e756560fc276315ac0b9c669e0067
AhongBOSS <ahongboss12@gmail.com>
2025-03-13 23:25:31 +0800

    新增LeetCode 2 做一半

---
+++ C/LeetCode/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct node
+{
+    int val;
+    struct node *next;
+}Node;
+
+typedef Node * NodePtr;
+
+struct NodePtr * R()
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) 
+{
+    NodePtr sNodePtr = NULL;
+
+    NodePtr newPte = malloc(sizeof(l1) + sizeof(l2));
+
+    if(l1 && l2)
+    {
+
+    }
+
+    else
+    {
+        return 0;
+    }
+}
