74bbaa25c57618b6ba1fe14ad0554b48630c2295
AhongBOSS <ahongboss12@gmail.com>
2025-03-22 23:44:33 +0800

    新增隨意寫的code

---
+++ C++/Practise/2025_03_22.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+
+class R
+{
+    public:
+        int len, wid;
+    
+    int A(int x = 0, int y = 0)
+    {
+        return x * y;
+    }
+    void B()
+    {
+        cout << "請輸入矩形的長度：";
+        cin >> len;
+    }
+    void C()
+    {
+        cout << "請輸入矩形的寬度：";
+        cin >> wid;
+    }
+    void D()
+    {
+        area = A(len, wid);
+         
+    }
+    void E()
+    {
+        cout << "矩形的長度為：" << len << endl;
+        cout << "矩形的寬度為：" << wid << endl;
+        cout << "矩形的面積為：" << area << endl;
+        cout << "矩形的周長為：" << per << endl;
+    }
+    int F(int x = 0, int y = 0)
+    {
+        return x * 2 + y * 2;
+    }
+    void G()
+    {
+        per = F(len, wid);
+        
+    }
+    
+    
+    private:
+        int area, per;
+        
+
+};
+
+int main()
+{
+    R r;
+    r.B();
+    r.C();
+    r.D();
+    r.F();
+    r.G();
+    r.E();
+    
+}
+
--- C/LeetCode/492.c
+++ C/LeetCode/492.c
@@ -2,6 +2,9 @@
 
 int* constructRectangle(int area, int* returnSize) 
 {
+    int a = 1 + area,sum[2];
+    
+    
     
 }
 
