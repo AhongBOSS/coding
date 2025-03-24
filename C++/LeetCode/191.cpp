7f780132cb42d6c3455b2bb11bba0ae82a6bfa4b
AhongBOSS <ahongboss12@gmail.com>
2025-03-24 23:32:33 +0800

    新增LeetCode 第191題C++

---
+++ C++/LeetCode/191.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        int hammingWeight(int n) 
+        {
+            int sum = 0;
+            while(n)
+            {
+                if(n % 2 == 1)
+                {
+                    sum++;
+                }
+                n/=2;
+            }
+            return sum;    
+        }
+    };
