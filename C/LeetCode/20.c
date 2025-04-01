8c8054bc9707ce3790351a7962584f3a706b4d91
AhongBOSS <ahongboss12@gmail.com>
2025-04-01 23:40:18 +0800

    新增LeetCode 第20題C

---
+++ C/LeetCode/20.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+bool isValid(char* s) 
+{
+    int a = 0;
+    for(int i = 0; s[i]; i++)
+    {
+        char b = s[i];
+        if(b == '(')
+        {
+            s[a++] = ')';
+        }
+        else if (b == '[')
+        {
+            s[a++] = ']';
+        }
+        else if (b == '{')
+        {
+            s[a++] = '}';
+        }
+        else if (a == 0 || s[--a] != b)
+        {
+            return false;
+        }
+    }
+    return true;
+
+
+}
